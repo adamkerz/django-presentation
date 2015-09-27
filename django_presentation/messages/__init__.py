6fd0fcbb799b287cd231297a27f27665d4de80b6
Adam Kerz <github@kerz.id.au>
2015-09-27 11:13:47 +1000

    Adding messages presentation support

---
+++ django_presentation/messages/__init__.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class MessagesConfig(AppConfig):
+    name='django_presentation.messages'
+    label='django_presentation_messages'
---
+++ django_presentation/messages/jinja2/__init__.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class MessagesConfig(AppConfig):
+    name='django_presentation.messages.jinja2'
+    label='django_presentation_messages_jinja2'
