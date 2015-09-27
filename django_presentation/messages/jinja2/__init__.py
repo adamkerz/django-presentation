5b298f5bc28d60bce595c0046e8c12930b694130
Adam Kerz <github@kerz.id.au>
2015-09-27 11:27:13 +1000

    Simplifying messageBox global function

--- django_presentation/messages/jinja2/__init__.py
+++ django_presentation/messages/jinja2/__init__.py
@@ -1,5 +1,7 @@
-from django.apps import AppConfig
+from django.utils.safestring import mark_safe as S
+from django.template.loader import get_template
+from django.template import Context
 
-class MessagesConfig(AppConfig):
-    name='django_presentation.messages.jinja2'
-    label='django_presentation_messages_jinja2'
+
+def messageBox(messages):
+    return S(get_template('messages/messageBox.html').render(Context({'messages':messages})))
--- django_presentation/messages/jinja2/context_processors.py
+++
@@ -1,9 +0,0 @@
-from django.utils.safestring import mark_safe as S
-from django.template.loader import get_template
-from django.template import Context
-
-
-def messageBox(request):
-    def fn(messages):
-        return S(get_template('messages/messageBox.html').render(Context({'messages':messages})))
-    return {'messageBox':fn}
