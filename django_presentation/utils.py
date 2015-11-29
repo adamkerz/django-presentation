2f17aebaa1db92fc4dfebce306bd08e6443201c2
Adam Kerz <github@kerz.id.au>
2015-11-30 07:48:07 +1000

    Only index if index is not None

--- django_presentation/utils.py
+++ django_presentation/utils.py
@@ -15,9 +15,9 @@ def interpretValue(value,*args,**kwargs):
 def specialInterpretValue(value,index,*args,**kwargs):
     """Interprets a passed value. In this order:
      - If it's callable, call it with the parameters provided
-     - If it's a tuple/list/dict, look up index within the tuple/list/dict
+     - If it's a tuple/list/dict and index is not None, look up index within the tuple/list/dict
      - Else, just return it
      """
     if callable(value): return value(*args,**kwargs)
-    if isinstance(value,tuple) or isinstance(value,list) or isinstance(value,dict): return value[index]
+    if index is not None and (isinstance(value,tuple) or isinstance(value,list) or isinstance(value,dict)): return value[index]
     return value
