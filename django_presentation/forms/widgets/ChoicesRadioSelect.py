a8134f692ddb0418bb56c3a7cd5f9139e03f1c25
Adam Kerz <github@kerz.id.au>
2016-01-21 11:10:51 +1000

    Fixing 1.9 deprecated import path

--- django_presentation/forms/widgets/ChoicesRadioSelect.py
+++ django_presentation/forms/widgets/ChoicesRadioSelect.py
@@ -1,5 +1,5 @@
 from django.forms import widgets
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe as S
 
